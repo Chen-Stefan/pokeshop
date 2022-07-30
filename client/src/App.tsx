37f6069f518eeb020c9b1c0fbb273704950aa5e6
Stefan Chen <zchen158@my.bcit.ca>
2022-07-30 14:16:58 -0700

    Create pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import './App.css';
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import 'App.css';
+import Context, { myContext } from "./pages/Context";
+import Register from "./pages/Register";
 
 function App() {
   return (
