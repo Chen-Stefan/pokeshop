3af19eb3bfba3ca9e8742c62d7c62dda44743464
Stefan Chen <zchen158@my.bcit.ca>
2022-07-31 16:58:42 -0700

    Fix App.tsx in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
 import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/HomePage";
-// import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import 'App.css';
+import "./styles/App.css";
 import Context, { myContext } from "./pages/Context";
-import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import Homepage from "./pages/HomePage";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Checkout from "./pages/Checkout";
 
 function App() {
+  const ctx = useContext(myContext);
+
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Homepage />}></Route>
-      {ctx ? (
-        <>
-          {ctx.isAdmin ? (
-            <Route path="/admin" element={<AdminPage />}></Route>
-          ) : null}
-          <Route path="/profile" element={<Profile />}></Route>
-        </>
-      ) : (
-        <>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </>
-      )}
-    </Routes>
-  </BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
