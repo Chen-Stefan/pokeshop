76406859210a0fe77f2cb955ba7fae918e846d17
Stefan Chen <zchen158@my.bcit.ca>
2022-08-03 14:41:17 -0700

    Delete unnecessary index.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './styles/index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
