f242fc0706d3f9786c6b4299a7f84e2109691b0b
Stefan Chen <zchen158@my.bcit.ca>
2022-08-04 14:46:23 -0700

    Add useState hooks

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useRef, useState, useEffect } from "react";
+
+// REGEX expression to validate username and password
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+  return <div>Register</div>;
 }
