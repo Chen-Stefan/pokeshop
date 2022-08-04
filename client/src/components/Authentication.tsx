fe9f3f3fe65300bbe5c05fd648b43182239da855
Stefan Chen <zchen158@my.bcit.ca>
2022-08-04 15:43:36 -0700

    Add login area to authentication component

--- client/src/components/Authentication.tsx
+++ client/src/components/Authentication.tsx
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Authentication() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="row mt-5">
       <div className="col">
-        <div className="jumbotron jumbotron-fluid mt-3" style= {{ minWidth: "18rem", marginBottom: "0" }}>
+        <div
+          className="jumbotron jumbotron-fluid mt-3"
+          style={{ minWidth: "18rem", marginBottom: "0" }}
+        >
           <div className="container">
             <h1 className="my-2 mx-4">Pokeshop</h1>
             <p className="lead mx-4 mt-4">Gather your favorite Pokemon Cards</p>
           </div>
+        </div>
+      </div>
+
+      <div className="col">
+        <div className="jumbotron pt-3 mt-3" style={{ minWidth: "18rem" }}>
+          <form action="">
+            <div className="form-floating my-3">
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="username"
+                required
+              />
+              <label htmlFor="username">Username</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="password"
+                required
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button
+              type="submit"
+              style={{ backgroundColor: "#350661" }}
+              className="btn btn-primary"
+            >
+              Login
+            </button>
+            <a
+              href="user/regist/er"
+              className="btn btn-outline-primary"
+              role="button"
+            >
+              Register
+            </a>
+          </form>
+          <span id="error-message"></span>
         </div>
       </div>
     </div>
