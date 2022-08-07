33eb98a528e7c292fcbe7adfdef60b30fe5ec6b3
Stefan Chen <zchen158@my.bcit.ca>
2022-08-07 15:41:14 -0700

    Create footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="logo_container">
+      <img
+        src="../assets/images/twitter.png"
+        alt="twitter"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="../assets/images/facebook.png"
+        alt="facebook"
+        width="50px"
+        height="50px"
+      />
+      <img
+        src="../assets/images/instagram.png"
+        alt="instagram"
+        width="50px"
+        height="50px"
+      />
+    </div>
+  );
+}
