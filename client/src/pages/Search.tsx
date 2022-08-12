6531df525ba24c64d40832b74b9dab0873915436
Stefan Chen <zchen158@my.bcit.ca>
2022-08-11 22:51:59 -0700

    Create filter and search JSX and style fonts

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export default function Search() {
+  const handleFilterByType = () => {};
+
+  const handleFilterByRegion = () => {};
+
+  const handleSearchByName = () => {};
+
   return (
-    <div>Search</div>
-  )
+    <>
+      <div className="filter__container justify-content-center">
+        <div className="filter__items">
+          <div>Type</div>
+          <select
+            value="{this.props.valuetype}"
+            onChange={handleFilterByType}
+          ></select>
+        </div>
+
+        <div className="filter__items">
+          <div>Region</div>
+          <select
+            value="{this.props.valueregion}"
+            onChange={handleFilterByRegion}
+          ></select>
+        </div>
+
+        <div className="filter__items">
+          <label>Search by name</label>
+          <input
+            type="text"
+            value=""
+            onChange={handleSearchByName}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
