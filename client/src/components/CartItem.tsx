86ba0da9832dcae1479dd3d92a6e45477481b37a
Stefan Chen <zchen158@my.bcit.ca>
2022-09-04 23:39:45 -0700

    Complete client side code for stripe

--- client/src/components/CartItem.tsx
+++ client/src/components/CartItem.tsx
@@ -16,49 +16,64 @@ export function CartItem({ id, quantity, totalPrice }: CartItemProps) {
 
   const handleCreateCheckout = () => {
     // We want to get back our url so that user can access our page
-    fetch('/create-checkout-session', {
-      method: 'POST',
+    fetch("/create-checkout-session", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: []
+      body: JSON.stringify({
+        items: [
+          { id: 1, quantity: 3 },
+          { id: 2, quantity: 1 },
+        ],
+      }),
+    }).then(res => {
+      if (res.ok) return res.json()
+      return res.json().then(json => Promise.reject(json))
+    }).then(({ url }) => {
+      // 试试 navigate
+      window.location = url
+    }).catch(e => {
+      console.error(e.error)
     })
-  }
+  };
 
   return (
-    <><Stack direction="horizontal" className="d-flex">
-      <img
-        src={item.imgUrl}
-        style={{ width: "125px", height: "120px", objectFit: "cover" }} />
-      <div className="me-auto">
-        <div>
-          {item.name}{" "}
-          <div className="text-muted" style={{ fontSize: ".65rem" }}>
-            x{quantity}
+    <>
+      <Stack direction="horizontal" className="d-flex">
+        <img
+          src={item.imgUrl}
+          style={{ width: "125px", height: "120px", objectFit: "cover" }}
+        />
+        <div className="me-auto">
+          <div>
+            {item.name}{" "}
+            <div className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </div>
+          </div>
+          <div className="text-muted" style={{ fontSize: ".75rem" }}>
+            {formatCurrency(item.price)}
           </div>
         </div>
-        <div className="text-muted" style={{ fontSize: ".75rem" }}>
-          {formatCurrency(item.price)}
-        </div>
-      </div>
-      <div>{formatCurrency(item.price * quantity)}</div>
-      <Button
-        variant="outline-danger"
-        size="sm"
-        className="float-end m-1"
-        onClick={() => removeFromCart(item.id)}
-      >
-        &times;
-      </Button>
-    </Stack><div className="ms-auto fw-bold fs-5">
+        <div>{formatCurrency(item.price * quantity)}</div>
+        <Button
+          variant="outline-danger"
+          size="sm"
+          className="float-end m-1"
+          onClick={() => removeFromCart(item.id)}
+        >
+          &times;
+        </Button>
+      </Stack>
+      <div className="ms-auto fw-bold fs-5">
         Total CAD {formatCurrency(totalPrice)}
       </div>
-    {totalPrice !== 0 && (
-      <button onClick={handleCreateCheckout} className="checkout">
-        Checkout
-      </button>
-      
-    )}
+      {totalPrice !== 0 && (
+        <button onClick={handleCreateCheckout} className="checkout">
+          Checkout
+        </button>
+      )}
     </>
   );
 }
